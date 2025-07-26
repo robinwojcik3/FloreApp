811f5413ae58c9f60a6f149da4a3ff8372bbf205
robinwojcik3 <robin.wojcik3@gmail.com>
2025-07-26 16:44:49 +0200

    Improve PDF extraction quality

--- app.js
+++ app.js
@@ -483,7 +483,10 @@ window.handleFloraGallicaClick = async function(event, pdfFile, startPage) {
             const [pg] = await newDoc.copyPages(srcDoc, [p - 1]);
             newDoc.addPage(pg);
         }
-        const newBytes = await newDoc.save();
+        const newBytes = await newDoc.save({
+            useObjectStreams: false,
+            compress: false
+        });
         const url = URL.createObjectURL(new Blob([newBytes], { type: 'application/pdf' }));
         window.open(`viewer.html?file=${encodeURIComponent(url)}`, '_blank');
     } catch (err) {
